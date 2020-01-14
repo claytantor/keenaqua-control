94bd16f6468a415fe439bdaae3b030e71ea1e19a
Clay Graham <claytantor@gmail.com>
2020-01-13 21:51:02 -0800

    getting rady to move to relay

--- README.md
+++ README.md
@@ -4,6 +4,7 @@
 python3 -m venv venv --system-site-packages
 curl https://bootstrap.pypa.io/get-pip.py | python3 
 pip install RPi.GPIO
+pip install -r requirements.txt
 ```
 
 
@@ -13,4 +14,18 @@ requests==2.21.0
 RPi.GPIO==0.7.0
 six==1.12.0
 urllib3==1.24.1
+```
+
+# creating the systemd service
+```
+$(pwd)/venv/bin/python3 makeservice.py -d $(pwd) -t keenaqua.service.mustache > keenaqua.service
+```
+
+Instructions for setting up your service can be found at https://www.raspberrypi-spy.co.uk/2015/10/how-to-autorun-a-python-script-on-boot-using-systemd/
+
+```
+sudo cp keenaqua.service /lib/systemd/system/keenaqua.service
+sudo chmod 644 /lib/systemd/system/keenaqua.service
+sudo systemctl daemon-reload
+sudo systemctl enable keenaqua.service
 ```
--- app.py
+++ app.py
@@ -1,66 +1,116 @@
-import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
+import RPi.GPIO as GPIO
+import sys
+import os
+import yaml
+import argparse
+import schedule
 
-import sys, os
+from time import sleep  # Import the sleep function from the time module
 
-from time import sleep # Import the sleep function from the time module
+GPIO.setwarnings(False)  # Ignore warning for now
+GPIO.setmode(GPIO.BCM)  # Use BCM pin numbering
 
-GPIO.setwarnings(False) # Ignore warning for now
-GPIO.setmode(GPIO.BCM) #Use BCM pin numbering
-
-relays = ["GREEN_LED","BLUE_LED", "RED_LED"]
+relays = ["GREEN_LED", "BLUE_LED", "RED_LED"]
 
 state = {
-	"GREEN_LED":{
-		"relay": 1,
-		"GPIO": 26,
-		"state": GPIO.LOW	
-	},
-	"BLUE_LED":{
-		"relay": 2,
-		"GPIO": 20,
-		"state": GPIO.LOW	
-	},
-	"RED_LED":{
-		"relay": 3,
-		"GPIO": 21,
-		"state": GPIO.LOW	
-	}
+    "GREEN_LED": {
+        "relay": 1,
+        "GPIO": 26,
+        "state": GPIO.LOW
+    },
+    "BLUE_LED": {
+        "relay": 2,
+        "GPIO": 20,
+        "state": GPIO.LOW
+    },
+    "RED_LED": {
+        "relay": 3,
+        "GPIO": 21,
+        "state": GPIO.LOW
+    }
 }
 
-  # Set pin 26 to be an output pin and set initial value to low (off)
-
 def setup_relays(relays):
-	for relay in relays:
-		GPIO.setup(state[relay]["GPIO"], GPIO.OUT, initial=GPIO.LOW)
-
+    for relay in relays:
+        print("setup_relay", relay, state[relay]["GPIO"] )
+        GPIO.setup(state[relay]["GPIO"], GPIO.OUT, initial=GPIO.LOW)
 
 def toggle_state(name, state):
-	state_element = state[name]
-	if state_element["state"] == GPIO.LOW:
-		state[name]["state"] = GPIO.HIGH
-	else:
-	    state[name]["state"] = GPIO.LOW
-	   
+    state_element = state[name]
+    if state_element["state"] == GPIO.LOW:
+        state[name]["state"] = GPIO.HIGH
+    else:
+        state[name]["state"] = GPIO.LOW
+
+
 def set_pin(name, state):
-	pin = state[name]["GPIO"]
-	state_value = state[name]["state"]
-	GPIO.output(pin, state_value) # set state	
+    print(name, state)
+    pin = state[name]["GPIO"]
+    state_value = state[name]["state"]
+    GPIO.output(pin, state_value)  # set state
+
 
 def toggle_pin(name, state):
-	toggle_state(name, state)
-	set_pin(name, state)
+    toggle_state(name, state)
+    set_pin(name, state)
+
+def keenaqua_job(device, schedule_item):
+
+    lookup = {
+        "on": GPIO.HIGH,
+        "off": GPIO.LOW
+    }
+
+    #set_pin(device["relay_name"], lookup[schedule_item["command"]])
+    # state[device["relay_name"]]["state"] = lookup[schedule_item["command"]]
+    # set_pin(device["relay_name"], state[device["relay_name"]]["state"])
+
+    print("job ==== ", 
+        device["relay_name"], 
+        state[device["relay_name"]]["GPIO"],
+        schedule_item["command"])
+
+    GPIO.output(state[device["relay_name"]]["GPIO"], lookup[schedule_item["command"]])
+    
+
+def loadConfig(configFile):
+    cfg = None
+    with open(configFile, 'r') as ymlfile:
+        cfg = yaml.load(ymlfile, Loader=yaml.FullLoader)
+    return cfg
 
 
 def main(argv):
     print("starting keenaqua control app.")
 
+    # Read in command-line parameters
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("-c", "--config", action="store",
+                        required=True, dest="config", help="the YAML configuration file")
+
+    args = parser.parse_args()
+    config = loadConfig(args.config)['keenaqua-scheduler']
+    print(config)
+
     setup_relays(relays)
 
-    while True: # Run forever
-        for relay in relays:
-            toggle_pin(relay, state)
-            sleep(1) # Sleep for 1 second
+    for device in config['devices']: 
+        print(device['name'], device['schedule']) 
+        for schedule_item in device['schedule']:
+            if(schedule_item['rate'] == 'day'):
+                schedule.every().day.at(schedule_item['value']).do(keenaqua_job, device=device, schedule_item=schedule_item )
+            if(schedule_item['rate'] == 'hours'):
+                schedule.every(schedule_item['value']).hours.do(keenaqua_job, device=device, schedule_item=schedule_item )
+            if(schedule_item['rate'] == 'minutes'):
+                schedule.every(schedule_item['value']).minutes.do(keenaqua_job, device=device, schedule_item=schedule_item )    
+
+    while True:  # Run forever
+        schedule.run_pending()
+        # for relay in relays:
+        #     toggle_pin(relay, state)
+        #     sleep(1)  # Sleep for 1 second
+
 
 if __name__ == "__main__":
     main(sys.argv[1:])
-
